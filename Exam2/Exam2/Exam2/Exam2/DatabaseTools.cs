a8ba134797a73b632460a6c7cf74c782687dbf55
TheTFoster <58485707+TheTFoster@users.noreply.github.com>
2023-02-06 16:02:19 -0600

    Add files via upload

---
+++ Exam2/Exam2/Exam2/Exam2/DatabaseTools.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using System.Windows.Forms;
+
+namespace Exam2
+{
+    public class DatabaseTools
+    {
+        public static SqlConnection GetConn()
+        {
+            string sqlConn = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\Premiere.mdf;Integrated Security=True";
+            SqlConnection connect = new SqlConnection(sqlConn);
+            return connect;
+        }
+        public static void DisplayData(DataGridView dGv, string sqlQuery)
+        {
+            SqlConnection con = DatabaseTools.GetConn();
+            con.Open();
+            SqlDataAdapter dataADAPT = new SqlDataAdapter(sqlQuery, con);
+            DataTable tble = new DataTable();
+            dataADAPT.Fill(tble);
+            dGv.DataSource = tble;
+            con.Close();
+
+        }
+        public double Total (double x, double y)
+        {
+            return (x * y);
+        }
+    }
+}
---
+++ Exam2/Exam2/Exam2/Exam2/Form2.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Exam2
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DatabaseTools.DisplayData(dataGridView1, "Select Orders.CustomerNum, Orders.OrderDate, Orders.OrderNum, OrderLine.NumOrdered, OrderLine.QuotedPrice, Part.PartNum, Part.[Description] from Orders inner join OrderLine on Orders.OrderNum = OrderLine.OrderNum inner join Part on Part.PartNum = OrderLine.PartNum");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //MessageBox.Show()
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlConnection con = DatabaseTools.GetConn();
+                con.Open();
+                SqlDataAdapter dataADAPT = new SqlDataAdapter();
+
+                dataADAPT.InsertCommand = new SqlCommand("insert into Orders(OrderNum, OrderDate, CustomerNum),Part(PartNum, Description, OnHand, Class, Warehouse, Price),OrderLine(NumOrdered,QuotedPrice) Values @ordernum,@orderdate,@custonum,@numordered,@quoteprice,@partnum,@partdesc,@onhand,@class,@warehouse,@price");
+                dataADAPT.InsertCommand.Parameters.AddWithValue("@ordernum", textBox1.Text);
+                dataADAPT.InsertCommand.Parameters.AddWithValue("@orderdate", textBox2.Text);
+                dataADAPT.InsertCommand.Parameters.AddWithValue("@custonum", textBox3.Text);
+                dataADAPT.InsertCommand.Parameters.AddWithValue("@numordered",Decimal.Parse(textBox4.Text));
+                dataADAPT.InsertCommand.Parameters.AddWithValue("@quotedprice",Decimal.Parse(textBox5.Text));
+                dataADAPT.InsertCommand.Parameters.AddWithValue("@partnum", textBox6.Text);
+                dataADAPT.InsertCommand.Parameters.AddWithValue("@onhand",Decimal.Parse(textBox7.Text));
+                dataADAPT.InsertCommand.Parameters.AddWithValue("@class", textBox8.Text);
+                dataADAPT.InsertCommand.Parameters.AddWithValue("@warehouse", textBox9.Text);
+                dataADAPT.InsertCommand.Parameters.AddWithValue("@price", Decimal.Parse(textBox10.Text));
+                dataADAPT.InsertCommand.ExecuteNonQuery();
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            DatabaseTools.DisplayData(dataGridView1, "select * from Orders,OrderLine,Part");
+        }
+    }
+}
